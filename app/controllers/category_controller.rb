98bcc54033b1b3adf7e058bfbac3066d48b9259d
jordimairena <jordim95@hotmail.com>
2018-02-22 23:38:19 -0600

    modificaciones

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,21 +1,42 @@
-class CategoryController < ApplicationController
-	def index
-  	@category = Category.all.order('created_at DESC')
+class CategoriesController < ApplicationController
+  def index
+    @category = Category.all.order('created_at DESC')
   end
+
+
   def new 
+    @category = Category.new
   end
 
   def create
-  	@category = Category.new(post_params)
-  	@category.save
+    @category = Category.new(post_params)
+    if @category.save
+      reddirect_to @category
+    else 
+      render 'new'
+    end
+  end
 
-  	redirect_to @category
-  end
   def show
-  	@category = Category.find(params[:id])
+    @category = Category.find(params[:id])
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(params[:category].permit(:name))
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
-  	def post_params
-  		params.require(:category).permit(:name)
-  	end
+    def post_params
+      params.require(:category).permit(:name)
+    end
 end
+©
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   
-  resources :category
+  resources :categories
 =begin 
  	get "/category" index
  	post "/category" create
