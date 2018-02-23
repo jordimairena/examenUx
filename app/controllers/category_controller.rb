be5afe66212743395b51afb2412030994f6e9502
jordimairena <jordim95@hotmail.com>
2018-02-22 23:45:09 -0600

    arreglos

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,8 +2,6 @@ class CategoriesController < ApplicationController
   def index
     @category = Category.all.order('created_at DESC')
   end
-
-
   def new 
     @category = Category.new
   end
@@ -11,7 +9,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(post_params)
     if @category.save
-      reddirect_to @category
+      redirect_to @category
     else 
       render 'new'
     end
@@ -34,9 +32,13 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
   private
     def post_params
       params.require(:category).permit(:name)
     end
 end
-©
